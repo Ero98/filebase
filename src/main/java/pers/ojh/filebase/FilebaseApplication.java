445c0cfcfd7324439752acd7b40d1cb465ab9370
CavorKehl <oujghg@163.com>
2021-05-03 11:05:57 +0800

    add README file

---
+++ src/main/java/pers/ojh/filebase/FilebaseApplication.java
@@ -0,0 +1,13 @@
+package pers.ojh.filebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pers/ojh/filebase/FilebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.ojh.filebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
